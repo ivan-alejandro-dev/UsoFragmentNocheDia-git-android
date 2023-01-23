d68057eeed55a709daf11ffd90216d86417377bd
Fabio Pereira <fabio.pereira@wirsolut.com>
2023-01-23 20:42:49 -0300

    Creacion del Proyecto

---
+++ app/src/main/java/com/example/usofragmentnochedia/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.usofragmentnochedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnModo;
+    boolean modoDia = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnModo = findViewById(R.id.btnModo);
+
+        getSupportFragmentManager()// libreria
+                .beginTransaction() // metodos
+                .add(R.id.contenedor, new ModoDia())// metodos
+                .commit();// metodos
+
+    }
+
+    public void cambiaModo(View view) {
+
+        Fragment f;
+
+        if (modoDia) {
+            f = new ModoNoche();
+        } else {
+            f = new ModoDia();
+        }
+        modoDia = !modoDia;
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.contenedor, f)
+                .commit();
+
+    }
+}
